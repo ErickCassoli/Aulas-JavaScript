4a9b9ae0285ba494f2c8c0ac1c0455326fb45870
ErickCassoli <erick.mcassoli@gmail.com>
2023-03-02 22:23:10 -0300

    02/03/2023

---
+++ vetores/vetores.js
@@ -0,0 +1,19 @@
+let n1 = [3,4,1,5,9]
+/*let n2 = []*/
+
+for(let i = n1.length - 1 ; i >= 0 ; i--)
+    n1.push(n1[i])
+    n1.
+console.log(n1)
+/*
+for(let i = n1.length - 1 ; i >= 0 ; i--){
+    n2.push(n1[i])
+}
+*/
+
+/*
+for(let i = 1 ; i < n1.length ; i++){
+    n2[n1.length] = n1[i-1]
+}
+
+*/
